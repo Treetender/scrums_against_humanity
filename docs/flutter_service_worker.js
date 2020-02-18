1fca06c85832613f5b34c29fbe77b24cc39599fb
treetender <treetender@gmail.com>
2020-02-17 21:57:49 -0500

    updating gitpages sw paths

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -1,16 +1,16 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/main.dart.js": "2b8ad205368ef7f6ad48fba98f2b70c1",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/LICENSE": "0b27cc7d67a68cb7938ac2dae093052c",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "7d3907ecb9a5d5953c48a5eb6e87c04e",
-"/manifest.json": "8dcc0201e2555b9ed9544827a6b1938d"
+  "main.dart.js": "2b8ad205368ef7f6ad48fba98f2b70c1",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/LICENSE": "0b27cc7d67a68cb7938ac2dae093052c",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "7d3907ecb9a5d5953c48a5eb6e87c04e",
+"manifest.json": "8dcc0201e2555b9ed9544827a6b1938d"
 };
 
 self.addEventListener('activate', function (event) {
